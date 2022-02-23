56a5fdae2bfc419d42fc3bc4445eedd3eae7655e
WozardLozard <jerstar36@gmail.com>
2022-02-23 09:45:27 -0700

    Update readme

--- src/client/actions/PresenceUpdate.js
+++ src/client/actions/PresenceUpdate.js
@@ -7,14 +7,17 @@ class PresenceUpdateAction extends Action {
   handle(data) {
     const c = this.client;
     const guild = c.guilds.cache.get(data.guild_id);
-    let presence = guild.presences.cache.get(data.user.id);
-    let old = null;
-    if (presence) {
-      old = presence._clone();
-    }
-    presence = guild.presences._add(Object.assign(data, { guild }), false);
-    if (c.listenerCount(Events.PRESENCE_UPDATE)) {
-      c.emit(Events.PRESENCE_UPDATE, old, presence);
+
+    if (guild) {
+      let presence = guild.presences.cache.get(data.user.id);
+      let old = null;
+      if (presence) {
+        old = presence._clone();
+      }
+      presence = guild.presences._add(Object.assign(data, { guild }), false);
+      if (c.listenerCount(Events.PRESENCE_UPDATE)) {
+        c.emit(Events.PRESENCE_UPDATE, old, presence);
+      }
     }
   }
 }
